2c05c2c9af813a48f1aa8de535b02c025bebca78
krongk <kenrome@gmail.com>
2012-10-31 21:14:42 +0800

    add qidai

--- config/routes.rb
+++ config/routes.rb
@@ -107,8 +107,8 @@ RailsOnWeb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  #root :to => 'home#index'
-  root :to => 'pages#index'
+  root :to => 'home#index'
+  #root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
