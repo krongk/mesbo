309da6f178ee2c08612fd2a083c0c3152a61f7ef
krongk <kenrome@gmail.com>
2013-08-05 20:45:14 +0800

    update jameng form

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,9 @@
 class Customer < ActiveRecord::Base
-	has_many :orders
+  has_many :orders
+
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :confirm, presence: true
+  validates :email, presence: true
+  validates :captcha, presence: true
 end
--- app/models/jiameng.rb
+++ app/models/jiameng.rb
@@ -1,2 +1,4 @@
 class Jiameng < ActiveRecord::Base
+  validates :name, presence: true
+  validates :phone, presence: true
 end
